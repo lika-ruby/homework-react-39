ae408733d85e6df2ede683153407f10d5519850a
lika-ruby <mary.havrylova.2011@gmail.com>
2026-05-26 21:53:19 +0300

    Updates

---
+++ static/js/312.dc90d522.chunk.js
@@ -0,0 +1,69 @@
+"use strict";(globalThis.webpackChunkhomework_react_34=globalThis.webpackChunkhomework_react_34||[]).push([[312],{312(o,e,r){r.r(e),r.d(e,{default:()=>b});var n=r(3),a=r(291),s=r(892),t=r(978),i=r(621),l=r(403);const p=l.Ay.div`
+  width: 700px;
+  margin: 0 auto;
+  padding: 108px 48px;
+  background: #101822;
+`,d=l.Ay.h2`
+  color: #ffffff;
+  font-size: 56px;
+  font-weight: 800;
+  text-align: center;
+  margin-bottom: 48px;
+`,f=(0,l.Ay)(s.lV)`
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+`,c=l.Ay.label`
+  color: #ffffff;
+  font-size: 28px;
+  font-weight: 700;
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+`,x=(0,l.Ay)(s.D0)`
+  width: 100%;
+  padding: 18px 24px;
+  background: #14202e;
+  border: 2px solid transparent;
+  color: #ffffff;
+  font-size: 20px;
+  font-weight: 500;
+  outline: none;
+  transition: 250ms;
+
+  &::placeholder {
+    color: rgba(255, 255, 255, 0.5);
+  }
+
+  &:focus {
+    border-color: #980000;
+  }
+`,m=(0,l.Ay)(s.Kw)`
+  color: #ff4d4d;
+  font-size: 18px;
+  font-weight: 500;
+`,h=l.Ay.p`
+  color: #ff4d4d;
+  font-size: 18px;
+  font-weight: 500;
+`,u=l.Ay.button`
+ width: 300px;
+  margin: 0 auto;
+  padding: 18px 24px;
+  background: #980000;
+  color: #ffffff;
+  font-size: 24px;
+  font-weight: 700;
+  cursor: pointer;
+  margin-top: 24px;
+  transition: 250ms;
+
+  &:hover {
+    background: #770000;
+  }
+
+  &:focus {
+    border-color: #ffffff;
+  }
+`;var g=r(579);const w=()=>{const o=(0,n.wA)(),e=(0,n.d4)(t.nT),r=(0,i.Zp)();return(0,g.jsxs)(p,{children:[(0,g.jsx)(d,{children:"Register"}),(0,g.jsx)(s.l1,{initialValues:{name:"",email:"",password:""},validate:o=>{const e={};return o.email?o.password?o.password.length<6?(e.password="Too short",e):o.password.length>16?(e.password="Too long",e):e:(e.password="Required",e):(e.email="Required",e)},onSubmit:(e,n)=>{o((0,a.kz)(e)).unwrap().then(()=>{r("/")}).catch(o=>{}),n.resetForm()},children:(0,g.jsxs)(f,{children:[(0,g.jsxs)(c,{children:["Your name",(0,g.jsx)(x,{type:"text",name:"name",placeholder:"Romana"})]}),(0,g.jsx)(m,{name:"name",component:"p"}),(0,g.jsxs)(c,{children:["Your email",(0,g.jsx)(x,{type:"email",name:"email",placeholder:"my.email@gmail.com"})]}),(0,g.jsx)(m,{name:"email",component:"p"}),(0,g.jsxs)(c,{children:["Your password",(0,g.jsx)(x,{type:"password",name:"password",placeholder:"**********"})]}),(0,g.jsx)(m,{name:"password",component:"p"}),e&&(0,g.jsx)(h,{children:"Your email or/and password are not suitable"}),(0,g.jsx)(u,{type:"submit",children:"Register"})]})})]})},b=()=>(0,g.jsx)("div",{children:(0,g.jsx)(w,{})})}}]);
+//# sourceMappingURL=312.dc90d522.chunk.js.map
